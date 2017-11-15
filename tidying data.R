be2f5618aea128432a1c281c2763487f1bd697cb
Darrick Emery <darrick.emery@stlcop.edu>
2017-11-15 12:34:37 -0600

    Revised Renaming and changing factors

--- tidying data.R
+++ tidying data.R
@@ -3,26 +3,16 @@ library(tidyverse)
 public_table <- read.csv("Public_Table11.csv")
 View(public_table)
 
-#Selecting for only revelant information in the table
+#Selecting for only revelant information in the table and renaming the column headers 
 
-public_table_sorted <- select(public_table, "xYear", "Schedule_Group", "Age_Group", "PillCount", "RxCount", "MeanPill_byRx")
+public_table_sorted <- select(public_table, "Year" = "xYear",
+                              "Schedule Group" = "Schedule_Group",
+                              "Age Range" = "Age_Group",
+                              "Pill Count" = "PillCount",
+                              "Prescription Count" = "RxCount",
+                              "Average Pill Count by Prescription" = "MeanPill_byRx")
 
 View(public_table_sorted)
 
-#Renaming column titles
-
-public_table_sorted %>% 
-  arrange(public_table_sorted, xYear, Schedule_Group) %>%
-  rename(public_table_sorted, "Year" = xYear)
-  
-public_table_sorted %>% 
-  arrange(public_table_sorted, xYear, Schedule_Group) %>%
-  rename(public_table_sorted, "Age Range" = Age_Group)
-
-public_table_sorted %>%
-  arrange(public_table_sorted, xYear, Schedule_Group) %>%
-  rename(public_table_sorted, "Schedule Group" = Schedule_Group)
-
-  
-  
-  
+public_table_sorted$"Age Range" <-  as.factor(public_table_sorted$`Age Range`)
+levels(public_table_sorted$"Age Range") <- c("14 years or younger", "65 years or older", "15 to 24 years", "25 to 44 years","45 to 64 years")
