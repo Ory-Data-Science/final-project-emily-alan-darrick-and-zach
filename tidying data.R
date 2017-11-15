2fab257091acd972647d1d9591d3775200e23c67
Darrick Emery <darrick.emery@stlcop.edu>
2017-11-15 11:44:11 -0600

    Renaming Columns

--- tidying data.R
+++ tidying data.R
@@ -1,13 +1,28 @@
-setwd("~/GitHub/final-project-emily-alan-darrick-and-zach/final-project-emily-alan-darrick-and-zach")
+setwd("~/GitHub/final-project-emily-alan-darrick-and-zach")
 library(tidyverse)
 public_table <- read.csv("Public_Table11.csv")
 View(public_table)
 
+#Selecting for only revelant information in the table
 
 public_table_sorted <- select(public_table, "xYear", "Schedule_Group", "Age_Group", "PillCount", "RxCount", "MeanPill_byRx")
 
 View(public_table_sorted)
 
+#Renaming column titles
+
 public_table_sorted %>% 
-  arrange(public_table_sorted, xYear, Schedule_Group)
-  rename(public_table_sorted, Year = xYear)
+  arrange(public_table_sorted, xYear, Schedule_Group) %>%
+  rename(public_table_sorted, "Year" = xYear)
+  
+public_table_sorted %>% 
+  arrange(public_table_sorted, xYear, Schedule_Group) %>%
+  rename(public_table_sorted, "Age Range" = Age_Group)
+
+public_table_sorted %>%
+  arrange(public_table_sorted, xYear, Schedule_Group) %>%
+  rename(public_table_sorted, "Schedule Group" = Schedule_Group)
+
+  
+  
+  
