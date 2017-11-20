dd53bcb71633a5f351ed9ee1562b91961062f5f7
emily-cunningham <31221851+emily-cunningham@users.noreply.github.com>
2017-11-20 11:59:00 -0600

    removing 2017 data

--- tidying data.R
+++ tidying data.R
@@ -1,13 +1,30 @@
-setwd("~/GitHub/final-project-emily-alan-darrick-and-zach/final-project-emily-alan-darrick-and-zach")
+
+
+#UPDATE WED
+
+setwd("~/GitHub/final-project-emily-alan-darrick-and-zach")
 library(tidyverse)
 public_table <- read.csv("Public_Table11.csv")
 View(public_table)
 
+#Selecting for only revelant information in the table and renaming the column headers 
 
-public_table_sorted <- select(public_table, "xYear", "Schedule_Group", "Age_Group", "PillCount", "RxCount", "MeanPill_byRx")
+public_table_sorted <- select(public_table, "Year" = "xYear",
+                              "Schedule Group" = "Schedule_Group",
+                              "Age Range" = "Age_Group",
+                              "Pill Count" = "PillCount",
+                              "Prescription Count" = "RxCount",
+                              "Average Pill Count by Prescription" = "MeanPill_byRx")
 
 View(public_table_sorted)
 
-public_table_sorted %>% 
-  arrange(public_table_sorted, xYear, Schedule_Group)
-  rename(public_table_sorted, Year = xYear)
+#Changing factor names
+public_table_sorted$"Age Range" <-  as.factor(public_table_sorted$`Age Range`)
+levels(public_table_sorted$"Age Range") <- c("14 years or younger", "65 years or older", "15 to 24 years", "25 to 44 years","45 to 64 years")
+
+View(public_table_sorted)
+
+filter(public_table_sorted, Year != "2017")
+View(public_table_sorted)
+
+?filter
