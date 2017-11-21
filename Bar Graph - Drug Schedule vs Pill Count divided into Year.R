bc9882a52329ee741b4fc679532935f4950ae6fc
alan-catalano <31221731+alan-catalano@users.noreply.github.com>
2017-11-20 22:02:19 -0600

    Drug Schedule vs Pill Count divided into Years

---
+++ Bar Graph - Drug Schedule vs Pill Count divided into Year.R
@@ -0,0 +1,8 @@
+#Drug Schedule vs # of pills divided into years
+#Can visually see correlation - decrease in pills dispensed each year
+ggplot(public_table, aes(Schedule_Group, PillCount, )) 
+  + geom_col() + facet_grid(. ~xYear) 
+  + labs( x ="Drug Schedule", y = "Pills Dispensed", title = "Pills Dispensed by Drug Schedule", subtitle = "Sorted by Year")
+
+
+
